047a238001a26e761fb76754558eeb662b94c2f2
Benjamin <benjaminrowe0@gmail.com>
2021-12-14 19:06:39 +1100

    have fixed the problem

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 2
+let num4 = 4
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num4 % 5
+let max = Math.max(0,20)
